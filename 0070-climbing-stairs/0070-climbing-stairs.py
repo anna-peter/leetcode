79d2d2bcd049d359bbff587531567f15ececb45a
Anna <81258938+anna-peter@users.noreply.github.com>
2026-07-04 22:44:07 +0100

    Time: 0 ms (100%), Space: 19.3 MB (51.82%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # build arr dp[] where dp[i] defines the number of ways to climb i steps
+        # solution will be dp[n]
+        # we build dp from the bottom up (more efficient)
+        if n <=2:
+            return n
+        dp = [0]*n
+        print(dp)
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2,n):
+            dp[i] = dp[i-1]+dp[i-2]
+       # dp[3] = dp[2] + dp[1] = 3
+       # dp[4] = 3 + 2 = 5 # 1,1,1,1 - 1,2,1 - 1,1,2 -2,2 - 2,1,1 
+        return dp[n-1]
